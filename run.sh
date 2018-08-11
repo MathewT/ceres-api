787d70951e3dcc28838fe96014d98ce467056d70
Mathew Thomas <mathew.thomas@longdivision.com>
2018-08-11 15:53:29 -0400

    fix run.sh
    Signed-off-by: Mathew Thomas <mathew.thomas@longdivision.com>

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# docker run -p 3000:3000 -p 3001:3001 -v "$(pwd)"/src:/usr/src/app/src -d  mthomas/node-api
-# docker run -p 8888:8888  mthomas/ceres-api
-
-docker run -p 8585:8585 --rm \
+docker run -d -p 8585:8585 --rm \
   --env-file ./app.env \
   --name "ceres-api" \
-  -v "$(pwd)"/src:/usr/src/app/src  mthomas/ceres-api
+  -v $HOME/.aws:/root/.aws -v $HOME/.ssh:/root/.ssh \
+  -v "$(pwd)"/src:/usr/src/app/src  mthomas/ceres-api 
